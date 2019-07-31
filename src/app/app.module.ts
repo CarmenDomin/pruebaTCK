c891b02ce800cafb20dc3277145aa904e604831d
Carmen Dominguez <mariadelcarmen.dominguezcampillo@kbc.be>
2019-07-31 08:49:54 +0200

    Add navigation component and viewport mixins

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule} from '@angular/router';
 
+import {NavigatorComponent} from './components/navigator/navigator.component';
+import {PageOneComponent} from './pages/page-one/page-one.component';
+import {PageTwoComponent} from './pages/page-two/page-two.component';
 import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavigatorComponent,
+    PageOneComponent,
+    PageTwoComponent,
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'page-one',
+        component: PageOneComponent,
+      },
+      {
+        path: 'page-two',
+        component: PageTwoComponent,
+      },
+      {
+        path: '**',
+        component: PageOneComponent,
+      }
+    ])
   ],
   providers: [],
   bootstrap: [
