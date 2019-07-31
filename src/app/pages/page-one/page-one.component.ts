b47a11a0862932a127525468f460fde7f00c0c79
Carmen Dominguez <mariadelcarmen.dominguezcampillo@kbc.be>
2019-07-31 08:58:41 +0200

    Add page-one

--- src/app/pages/page-one/page-one.component.ts
+++ src/app/pages/page-one/page-one.component.ts
@@ -5,4 +5,50 @@ import {Component} from '@angular/core';
   templateUrl: './page-one.component.html',
 })
 export class PageOneComponent {
+  public pensions = [
+    {
+      name: 'Solo Alojamiento',
+      characteristics: [
+        'Sin régimen',
+        'Botella de agua de bienvenida'
+      ]
+    },
+    {
+      name: 'Alojamiento y Desayuno',
+      characteristics: [
+        'Desayuno buffet completo'
+      ]
+    },
+    {
+      name: 'Media Pensión',
+      characteristics: [
+        'Desayuno buffet completo',
+        'Cena buffet',
+        'No incluye'
+      ]
+    },
+    {
+      name: 'Todo incluido',
+      characteristics: [
+        'Dispondrás de comida y bebida todo el día y durante toda tu estancia',
+        'Restaurante buffet',
+        'Snack Bar',
+        'Servicio de bares (marcas nacionales)'
+      ]
+    },
+    {
+      name: 'Unlimited Services',
+      characteristics: [
+        'Restaurante buffet con bebidas Premium',
+        'Restaurante a la carta (una cena por estancia)',
+        'Snack Bar con bebidas Premium',
+        'Minibar incluido',
+        'Acceso a SPA y 45 minutos de masaje por persona',
+        'Actividades deportivas (acuáticas sin motor)',
+        'Caja fuerte',
+        'Parking gratuito',
+        'Lavandería'
+      ]
+    }
+  ];
 }
