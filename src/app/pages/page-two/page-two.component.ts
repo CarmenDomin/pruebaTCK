c7deb571b02ced9d3780ff3a85cfb4d69724e4b5
Carmen Domínguez Campillo <va.carmend@gmail.com>
2019-08-01 21:10:20 +0200

    Add hover tiles and minor

--- src/app/pages/page-two/page-two.component.ts
+++ src/app/pages/page-two/page-two.component.ts
@@ -14,6 +14,7 @@ export class PageTwoComponent {
         {
           imageName: 'img1',
           title: 'Olvídese de la rutina',
+          collapsed: true,
           content: 'Cras vehicula vestibulum dapibus. Pellentesque auctor dolor et purus facilisis maximus. Mauris eget urna luctus, imperdiet neque nec, aliquet lorem.',
           moreInformation: {
             text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
@@ -41,10 +42,12 @@ export class PageTwoComponent {
     },
     {
       title: 'Servicios de habitación',
+      collapsed: false,
       cards: [
         {
           imageName: 'img1',
           title: 'Título',
+          collapsed: true,
           content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
           moreInformation: {
             text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
